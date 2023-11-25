d3798c8e1cbf0a7532d1e47697eb6c6b865c29f1
Fermin Ramos <ramosfer@unm.edu>
2023-11-25 16:30:13 -0700

    Added file dependency

--- main.c
+++ main.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
+#include "random437.h"
 
 int main() {
     printf("Hello, World!\n");
+    int myRandInt = poissonRandom(45);
+    printf("My Random Number = %d\n", myRandInt);
     return 0;
 }
